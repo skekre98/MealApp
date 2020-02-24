58659592f1f1e5b80c84c5665277b0a2713b7472
Sharvil Kekre <sharvilkekre@gmail.com>
2020-02-24 00:37:42 -0800

    Users table created

--- src/models.py
+++ src/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Users(models.Model):
-    firstname= models.CharField(max_length=100)
-    lastname= models.CharField(max_length=100)
+    name= models.CharField(max_length=100)
     email= models.CharField(max_length=200)
     password= models.CharField(max_length=200)
