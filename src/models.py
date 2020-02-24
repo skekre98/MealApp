32a1ed8d3f5f9ca53189ed629b602b19058f1314
Adi <adarsh.ramchandran@gmail.com>
2020-02-24 00:29:01 -0800

    created table in models

--- src/models.py
+++ src/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    firstname= models.CharField(max_length=100)
+    lastname= models.CharField(max_length=100)
+    email= models.CharField(max_length=200)
+    password= models.CharField(max_length=200)
