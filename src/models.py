b2b523279192332afe41c4567d2b7c1d768fe3cf
Adi <adarsh.ramchandran@gmail.com>
2020-02-24 01:00:28 -0800

    user table updated

--- src/models.py
+++ src/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Users(models.Model):
-    name= models.CharField(max_length=100)
-    email= models.CharField(max_length=200)
-    password= models.CharField(max_length=200)
-    weight= models.IntegerField() # pounds
-    height= models.IntegerField() # inches
-    gender= models.IntegerField() # 0:M or 1:F
-    goal= models.DecimalField(max_digits=3, decimal_places=2) # -0.5 represents losing 1/2 pound per week
-    lifestyle= models.IntegerField() # some number 1 through 5
-    vegetarian= models.BooleanField()
+    name= models.CharField(max_length=100, default= '')
+    email= models.CharField(max_length=200, default= '')
+    password= models.CharField(max_length=200, default= '')
+    weight= models.IntegerField(default= -1) # pounds
+    height= models.IntegerField(default= -1) # inches
+    gender= models.IntegerField(default= -1) # 0:M or 1:F
+    goal= models.DecimalField(max_digits=2, decimal_places=1, default= -1.0) # -0.5 represents losing 1/2 pound per week
+    lifestyle= models.IntegerField(default= -1) # some number 1 through 5
+    vegetarian= models.BooleanField(default= -1)
