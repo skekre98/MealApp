14b01d29f451cfaa100c477f265250bce6c10955
Adi <adarsh.ramchandran@gmail.com>
2020-02-24 00:47:29 -0800

    user table updated

--- src/models.py
+++ src/models.py
@@ -4,3 +4,9 @@ class Users(models.Model):
     name= models.CharField(max_length=100)
     email= models.CharField(max_length=200)
     password= models.CharField(max_length=200)
+    weight= models.IntegerField() # pounds
+    height= models.IntegerField() # inches
+    gender= models.CharField(max_length=1) # M or F
+    goal= models.DecimalField(max_digits=3) # -0.5 represents losing 1/2 pound per week
+    lifestyle= models.IntegerField() # some number 1 through 5
+    vegetarian= models.BooleanField()
