06bb6a2adf281ceec4a2694d3fece1e006c72965
Adi <adarsh.ramchandran@gmail.com>
2020-02-24 18:04:29 -0800

    added calorie intake goal and age field

--- src/views.py
+++ src/views.py
@@ -33,28 +33,44 @@ def ingest_register(request):
     # TODO
     print(request.POST)
     if request.method == 'POST':
-        req_list = [request.POST.get('name'), request.POST.get('email'), request.POST.get('password'), request.POST.get('weight'), request.POST.get('height'), request.POST.get('gender'), request.POST.get('goal'), request.POST.get('lifestyle'), request.POST.get('vegetarian')]
+        req_list = [request.POST.get('name'), request.POST.get('email'), request.POST.get('password'), request.POST.get('weight'), request.POST.get('feet'), request.POST.get('inches'), request.POST.get('gender'), request.POST.get('goal'), request.POST.get('lifestyle'), request.POST.get('vegetarian'), request.POST.get('age')]
         if all(r is not None for r in req_list):
             register=Users()
             register.name= req_list[0]
             register.email= req_list[1]
             register.password= req_list[2]
-            register.weight= req_list[3]          
-            register.height= req_list[4]
+            register.weight= int(req_list[3])
 
-            str_gender = req_list[5]
+            ft = int(req_list[4])
+            inch = int(req_list[5])      
+            register.height = 12*ft + inch
+
+            str_gender = req_list[6]
             if str_gender == 'Male':
                 register.gender= 0
             else:
                 register.gender= 1
 
-            register.goal= req_list[6]
+            register.goal= float(req_list[7][:4])
       
-            lifestyle_num = req_list[7]
-            mult_fac = [1.2, 1.375, 1.55, 1.725, 1.9]  
-            register.lifestyle = mult_fac[lifestyle_num-1]
+            lifestyle_dict = {'Sedentary': 1.2, 'Lightly Active': 1.375, 'Moderately Active': 1.55, 'Very Active': 1.725, 'Extremely Active': 1.9}
+            register.lifestyle = lifestyle_dict[req_list[8]]
 
-            register.vegetarian = req_list[8]
+            veg_res = req_list[9]
+            if veg_res == 'Yes':
+                register.vegetarian = True
+            else:
+                register.vegetarian = False
+
+            register.age = int(req_list[10])
+
+            cal_burned = 0
+            if str_gender == 'Male':
+                cal_burned = 66 + (6.23 * register.weight) + (12.7 * register.height) - (6.8 * register.age)
+            else:
+                cal_burned = 655 + (4.35 * register.weight) + (4.7 * register.height) - (4.7 * register.age)
+            register.calorie_intake = int(cal_burned + (500*register.goal))
+
             register.save()
             
             return render(request, 'register.html')  
