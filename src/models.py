81b7ecb6d50d3dae10789ad0173215b70ce82f35
Adi <adarsh.ramchandran@gmail.com>
2020-02-24 16:11:34 -0800

    migrations for users table

--- src/models.py
+++ src/models.py
@@ -9,4 +9,4 @@ class Users(models.Model):
     gender= models.IntegerField(default= -1) # 0:M or 1:F
     goal= models.DecimalField(max_digits=2, decimal_places=1, default= -1.0) # -0.5 represents losing 1/2 pound per week
     lifestyle= models.IntegerField(default= -1) # some number 1 through 5
-    vegetarian= models.BooleanField(default= False)
+    vegetarian= models.BooleanField(default= True)
